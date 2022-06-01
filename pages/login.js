c5ebc5c5f6bf2dbab3911a8ce158e622f40ce553
Rafael <rafaellima52468@gmail.com>
2022-06-01 14:39:27 -0300

    adicionando componentes internos

---
+++ pages/login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { AppContainer } from '../src/components/AppContainerComponent'
+import { AppInput } from '../src/components/AppInputComponent'
+import { AppButton } from '../src/components/AppButtonComponent'
+
+const Login = props => {
+  return (
+    <AppContainer>
+      <AppInput
+        title="apelido ou email do usuário"
+        label="Usuário:"
+        type="text"
+      />
+      <AppInput label="Senha:" type="password" title="senha do usuário" />
+      <AppButton title="Entrar" />
+    </AppContainer>
+  )
+}
+
+export default Login
